cc4d084727bca7a727a9bea9c5be9b4e2f01c448
Dave <dave.cadelina@outlook.com>
2021-05-30 21:19:22 +1000

    completed BST Traversal

--- medium/BST-traversal.js
+++ medium/BST-traversal.js
@@ -1,11 +1,44 @@
 function inOrderTraverse(tree, array) {
-// Write your code here.
-}
+    // Write your code here.
+      if(tree.left){
+          inOrderTraverse(tree.left, array)
+      }
+      
+      array.push(tree.value)
+      
+      if(tree.right) {
+          inOrderTraverse(tree.right, array)
+      }
+      
+      return array
+  }
   
-function preOrderTraverse(tree, array) {
-// Write your code here.
-}
-
-function postOrderTraverse(tree, array) {
-// Write your code here.
-}
+  function preOrderTraverse(tree, array) {
+    // Write your code here.
+      array.push(tree.value)
+      
+      if(tree.left){
+          preOrderTraverse(tree.left, array)
+      }
+      
+      if(tree.right) {
+          preOrderTraverse(tree.right, array)
+      }
+      
+      return array
+  }
+  
+  function postOrderTraverse(tree, array) {
+    // Write your code here.
+      if(tree.left){
+          postOrderTraverse(tree.left, array)
+      }
+      
+      if(tree.right) {
+          postOrderTraverse(tree.right, array)
+      }
+      
+      array.push(tree.value)
+      
+      return array
+  }
