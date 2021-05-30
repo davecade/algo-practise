40c11e48dd7c690de1b2e50f429c61d27a3dfa5a
Dave <dave.cadelina@outlook.com>
2021-05-31 00:57:21 +1000

    completed generate docuemtn

--- easy/generate-document.js
+++ easy/generate-document.js
@@ -1,4 +1,38 @@
 function generateDocument(characters, document) {
-    // Write your code here.
-    return false;
-  }
+  // Write your code here.
+	let char = characters.split("")
+	let doc = document.split("")
+	let charUnique = {}
+	let docUnique = {}
+	let docUniqueArray = []
+	
+	for(let i = 0; i<char.length; i++) {
+		if(charUnique[char[i]] > 0) {
+			charUnique[char[i]]++
+		} else {
+			charUnique[char[i]] = 1
+		}
+	}
+	
+	for(let i = 0; i<doc.length; i++) {
+		if(docUnique[doc[i]] > 0) {
+			docUnique[doc[i]]++
+		} else {
+			docUnique[doc[i]] = 1
+			docUniqueArray.push(doc[i])
+		}
+	}
+	
+	console.log(charUnique)
+	console.log(docUnique)
+	
+	for(let item of docUniqueArray) {
+		if(charUnique[item] >= docUnique[item]) {
+			//Do Nothing
+		} else {
+			return false
+		}
+	}
+
+  return true;
+}
