af015ed664fdbc472279ab225cf305124ab028b1
Dave <dave.cadelina@outlook.com>
2021-06-04 01:12:24 +1000

    completed nonconstturcible change

--- easy/non-constructible-change.js
+++ easy/non-constructible-change.js
@@ -1,4 +1,15 @@
 function nonConstructibleChange(coins) {
     // Write your code here.
-    return 1;
+      
+      let sortedCoins = coins.sort((a,b) => a-b)
+      console.log("sorted", sortedCoins)
+      let change = 0
+      
+      for(let coin of coins) {
+          if(coin > change+1) {
+              return change+1
+          }
+          change = change + coin
+      }
+    return change+1;
   }
