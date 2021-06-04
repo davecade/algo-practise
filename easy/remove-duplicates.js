7c6cbc9225e807c44131806f11f0d153697bcd74
Dave <dave.cadelina@outlook.com>
2021-06-05 02:05:50 +1000

    completed remove duplicates

--- easy/remove-duplicates.js
+++ easy/remove-duplicates.js
@@ -9,4 +9,26 @@ class LinkedList {
   function removeDuplicatesFromLinkedList(linkedList) {
     // Write your code here.
     return null;
+  }// This is an input class. Do not edit.
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
   }
+}
+
+function removeDuplicatesFromLinkedList(linkedList) {
+  // Write your code here.
+	let pointer = linkedList
+	while(pointer.next !== null) {
+		if(pointer.value === pointer.next.value) {
+			temp = pointer.next
+			pointer.next = pointer.next.next
+			temp = null
+		} else {
+			pointer = pointer.next
+		}
+	}
+	
+  return linkedList;
+}
