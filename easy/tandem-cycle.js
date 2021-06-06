4e9dc00c18ebc501f114df067686e35801f10e87
Dave <dave.cadelina@outlook.com>
2021-06-07 01:31:06 +1000

    completed tandem-cycle

--- easy/tandem-cycle.js
+++ easy/tandem-cycle.js
@@ -2,38 +2,23 @@ function tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest) {
     // Write your code here.
       let sortedRedShirtSpeeds
       let sortedBlueShirtSpeeds
-      let high = 0;
-      let low = 0;
+      let speed = 0;
       
-      let redShirtSpeedsSUM = redShirtSpeeds.reduce((a, b) => a + b, 0)
-      console.log("REDSUM", redShirtSpeedsSUM)
-      let blueShirtSpeedsSUM = blueShirtSpeeds.reduce((a, b) => a + b, 0)
-      console.log("BLUESUM", blueShirtSpeedsSUM)
-      
-      if(redShirtSpeedsSUM > blueShirtSpeedsSUM) {
+      if(fastest) {
           sortedRedShirtSpeeds = redShirtSpeeds.sort((a,b) => a-b)
           sortedBlueShirtSpeeds = blueShirtSpeeds.sort((a,b) => b-a)
       } else {
-          sortedRedShirtSpeeds = redShirtSpeeds.sort((a,b) => b-a)
+          sortedRedShirtSpeeds = redShirtSpeeds.sort((a,b) => a-b)
           sortedBlueShirtSpeeds = blueShirtSpeeds.sort((a,b) => a-b)
       }
       
-      
       for(let i = 0; i<sortedRedShirtSpeeds.length; i++) {
           if(sortedRedShirtSpeeds[i] >= sortedBlueShirtSpeeds[i]) {
-              high = high + sortedRedShirtSpeeds[i]
-              low = low + sortedBlueShirtSpeeds[i]
+              speed = speed + sortedRedShirtSpeeds[i]
           } else if(sortedRedShirtSpeeds[i] < sortedBlueShirtSpeeds[i]) {
-              high = high + sortedBlueShirtSpeeds[i]
-              low = low + sortedRedShirtSpeeds[i]
+              speed = speed + sortedBlueShirtSpeeds[i]
           }
       }
       
-      if(fastest) {
-          console.log("returned High")
-          return high;
-      } else {
-          console.log("returned Low")
-          return low;
-      }
+      return speed
   }
