affead66e2c2cf170ed4ab62543188a6f312f175
Dave <dave.cadelina@outlook.com>
2021-06-08 02:57:48 +1000

    copleted tournament winner

--- easy/tournament-winner.js
+++ easy/tournament-winner.js
@@ -1,4 +1,33 @@
 function tournamentWinner(competitions, results) {
-    // Write your code here.
-    return '';
-  }
+	let finalResults = {}
+	let highest = 0
+	let winner = ''
+	
+	//Get all Teams in cache
+	for(match of competitions) {
+		if(!finalResults[match[0]]) {
+			finalResults[match[0]] = 0
+		}
+		
+		if(!finalResults[match[1]]) {
+			finalResults[match[1]] = 0
+		}
+	}
+	
+	//tally results
+	for(let i = 0; i<competitions.length; i++) {
+		finalResults[competitions[i][results[i]===0?1:0]]++
+	}
+	
+	//Check which team has most points
+	let keys = Object.keys(finalResults);
+	for(let i=0; i<keys.length; i++) {
+		if(finalResults[keys[i]] > highest) {
+			highest = finalResults[keys[i]]
+			winner = keys[i]
+		}
+	}
+	
+	//Return the winner
+  return winner;
+}
