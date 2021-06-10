87d00ab693611428ad294eeea8fcfa67adf52cb5
Dave <dave.cadelina@outlook.com>
2021-06-11 02:50:06 +1000

    caclulated middle

--- medium/min-height-bst.js
+++ medium/min-height-bst.js
@@ -1,27 +1,29 @@
 function minHeightBst(array) {
-    // Write your code here.
+  // Write your code here.
+	let middle = array[Math.floor(array.length/2)]
+	
+}
+
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
-  
-  class BST {
-    constructor(value) {
-      this.value = value;
-      this.left = null;
-      this.right = null;
-    }
-  
-    insert(value) {
-      if (value < this.value) {
-        if (this.left === null) {
-          this.left = new BST(value);
-        } else {
-          this.left.insert(value);
-        }
+
+  insert(value) {
+    if (value < this.value) {
+      if (this.left === null) {
+        this.left = new BST(value);
       } else {
-        if (this.right === null) {
-          this.right = new BST(value);
-        } else {
-          this.right.insert(value);
-        }
+        this.left.insert(value);
+      }
+    } else {
+      if (this.right === null) {
+        this.right = new BST(value);
+      } else {
+        this.right.insert(value);
       }
     }
   }
+}
