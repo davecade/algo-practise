784a0a467c800f20406050aa63fe018f38c611d3
Dave <dave.cadelina@outlook.com>
2021-06-27 04:25:49 +1000

    better version of find kth largest value

--- medium/find-Kth-largest-value-in-BST.js
+++ medium/find-Kth-largest-value-in-BST.js
@@ -26,3 +26,37 @@ class BST {
       console.log(list)
     return list[(list.length)-k];
   }
+  
+
+  function findKthLargestValueInBst2(tree, k, list=[]) {
+    // Write your code here.
+      let result
+      
+      if(tree.left===null && tree.right===null) {
+          list.push(tree.value)
+          result = list[k-1]
+      } else {
+          
+          if(tree.right) {
+              result = findKthLargestValueInBst(tree.right, k, list)
+              
+              if(result) {
+                  return result
+              }
+          }
+          
+          list.push(tree.value)
+          result = list[k-1]
+          if(result) {
+              return list[k-1]
+          }
+          
+          if(tree.left) {
+              result = findKthLargestValueInBst(tree.left, k, list)
+              if(result) {
+                  return result
+              }
+          }
+      }
+    return result;
+  }
