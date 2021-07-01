c0f74876efdb1f1a09034b1762f858ea31e5953c
Dave <dave.cadelina@outlook.com>
2021-07-01 21:59:22 +1000

    removed unecessary code

--- medium/reconstruct-BST.js
+++ medium/reconstruct-BST.js
@@ -1,53 +1,46 @@
 // This is an input class. Do not edit.
 class BST {
-    constructor(value, left = null, right = null) {
-      this.value = value;
-      this.left = left;
-      this.right = right;
-    }
-  }
-  
-  function reconstructBst(preOrderTraversalValues) {
-    // Write your code here.
-      let array = preOrderTraversalValues
-      let root = new BST(array[0])
-      let pointer = root
-      let idx = 1
-      
-      while(array[idx] < array[idx-1]) {
-          pointer.left = new BST(array[idx])
-          idx++
-          pointer = pointer.left
+        constructor(value, left = null, right = null) {
+          this.value = value;
+          this.left = left;
+          this.right = right;
+        }
       }
       
-      while(idx<array.length) {
-          reconstructHelper(array[idx], root)
-          idx++
+      function reconstructBst(preOrderTraversalValues) {
+        // Write your code here.
+              let array = preOrderTraversalValues
+              let root = new BST(array[0])
+              let idx = 1
+      
+              while(idx<array.length) {
+                      reconstructHelper(array[idx], root)
+                      idx++
+              }
+              
+        return root;
       }
       
-    return root;
-  }
-  
-  
-  function reconstructHelper(value, root){
-          let finished = false;
-          let pointer = root
-  
-          while(!finished) {
-                  if(value < pointer.value) {
-                          if(pointer.left === null) {
-                                  pointer.left = new BST(value)
-                                  finished = true
-                          } else {
-                                  pointer = pointer.left
-                          }
-                  } else if(value >= pointer.value) {
-                          if(pointer.right === null) {
-                                  pointer.right = new BST(value)
-                                  finished = true
-                          } else {
-                                  pointer = pointer.right
-                          }
-                  }
-          }
-  }
+      
+      function reconstructHelper(value, root){
+        let finished = false;
+        let pointer = root
+
+        while(!finished) {
+                if(value < pointer.value) {
+                        if(pointer.left === null) {
+                                        pointer.left = new BST(value)
+                                        finished = true
+                        } else {
+                                        pointer = pointer.left
+                        }
+                } else if(value >= pointer.value) {
+                        if(pointer.right === null) {
+                                        pointer.right = new BST(value)
+                                        finished = true
+                        } else {
+                                        pointer = pointer.right
+                        }
+                }
+        }
+      }
